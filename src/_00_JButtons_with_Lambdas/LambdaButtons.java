af32964daa14b7d10f1a8b70833caaf9af3f52c6
Derek <Derek@LAPTOP-T10GJCDD>
2021-09-22 18:48:11 -0700

    done

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,11 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,7 +22,20 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((e)->{
+			String i=JOptionPane.showInputDialog("Give a number");
+			String j=JOptionPane.showInputDialog("Give another number");
+			int x=Integer.parseInt(i);
+			int y=Integer.parseInt(j);
+			System.out.println(x+y);
+			});
+		randNumber.addActionListener((e)->{
+			Random r = new Random();
+			System.out.println(r.nextInt());
+		});
+		tellAJoke.addActionListener((e)->{
+			System.out.println("This isn't a joke");
+		});
 		window.setVisible(true);
 		window.pack();
 	}
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,7 @@
 package _01_Lambda_Methods;
 
+import java.util.ArrayList;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -7,19 +9,66 @@ public class LambdaMethods {
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
 		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
+			for(int i = 0; i < 1; i++) {
 				System.out.println(s);
 			}
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((b)->{
+			String copy = b;
+			b="";
+			for (int i = copy.length()-1; i >= 0; i--) {
+				b+=copy.charAt(i);
+			}
+			System.out.println(b);
+		}, "word");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((c)->{
+			String copy = "";
+			for (int i = 0; i < c.length(); i++) {
+				if (i%2==0) {
+					copy+=Character.toUpperCase(c.charAt(i));
+				}else {
+					copy+=c.charAt(i);
+				}
+			}
+			System.out.println(copy);
+		}, "word");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((p)->{
+			String copy = "";
+			for (int i = 0; i < p.length(); i++) {
+				copy+=p.charAt(i);
+				if (i!=p.length()-1) {
+					copy+=".";
+				}
+			}
+			System.out.println(copy);
+		}, "word");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((p)->{
+			ArrayList<Character> c = new ArrayList<Character>();
+			boolean b = true;
+			c.add('a');
+			c.add('e');
+			c.add('i');
+			c.add('o');
+			c.add('u');
+			String copy = "";
+			for (int i = 0; i < p.length(); i++) {
+				for (int j = 0; j < c.size(); j++) {
+					if (p.charAt(i)==c.get(j)) {
+						b = false;
+					}
+				}
+				if(b==true) {
+					copy+=p.charAt(i);
+				}
+				b=true;
+			}
+			System.out.println(copy);
+		}, "word");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
